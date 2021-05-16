0f6d1005bd0f07eefe364977d244c937561741c7
Mr.Acid dev <81736710+mracid-devs@users.noreply.github.com>
2021-05-16 06:21:43 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 ///https://api.telegram.org/bot<token>/setwebhook?url=<url>
 
-$botToken = "1638003109:AAG9i44hrM4HuJVUiuEoLrSqXMWK6X1p7rw"; // Enter ur bot token
+$botToken = "1858367420:AAHkPsdzkIHCRGO4EBThtxwb_CNyV8RHTSU"; // Enter ur bot token
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
